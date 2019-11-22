1a9ef38426c19c4eb33c049688f000ece14a326d
PeterJochem <=>
2019-11-22 10:05:20 -0600

    Added changes to implement the paddle and the ball

--- Atari.py
+++ Atari.py
@@ -19,6 +19,67 @@ class brick:
         self.graphic.setFill(color)
         self.color = color
 
+class Ball:
+    
+    # Describe here
+    def init_Ball(self, game):
+
+        radius = float(game.window_height) / 70.0
+
+        x = float(game.window_width) / 2.0
+        y = game.window_height * 0.70
+
+
+        self.ball = Circle(Point(x , y), radius)
+        self.ball.setFill("white")
+
+        self.ball.draw(game.win)
+
+
+
+    def __init__(self, game):
+        self.alive = True
+        
+        self.game = game
+        self.ball = None
+        
+        self.init_Ball(self.game)
+
+
+# Describe here
+class Paddle:
+    
+    # Describe here
+    def init_Paddle(self):
+
+        paddleWidth = float(self.game.window_width) / 20.0
+        paddleHeight = float(self.game.window_height) / 100.0
+
+        paddle_Y = self.game.window_height * 0.80
+
+        x1 = ( float(self.game.window_width)  / 2.0) - (paddleWidth / 2.0)
+        y1 = paddle_Y - ( float( paddleHeight )  / 2.0)
+
+        x2 = ( float(self.game.window_width)  / 2.0) + (paddleWidth / 2.0)
+        y2 = paddle_Y + (float(paddleHeight) / 2.0) 
+
+        self.graphic = Rectangle(Point(x1, y1), Point(x2, y2) )
+        self.graphic.setFill("purple")
+        self.graphic.draw(self.game.win)
+
+    def __init__(self, game):
+
+        # Store the current location
+        self.leftX = 0
+        self.leftY = 0
+        
+        self.game = game
+        
+        self.init_Paddle() 
+    
+
+
+
 
 # This class describes a game and its info
 class game:
@@ -58,9 +119,40 @@ class game:
 
             self.bricks.append( currentRow  )
             
+    
+    # Describe here
+    def init_Ball(self):
+        
+        radius = float(self.window_height) / 70.0 
+         
+        x = float(self.window_width) / 2.0
+        y = self.window_height * 0.70 
+        
 
+        self.ball = Circle(Point(x , y), radius)
+        self.ball.setFill("white")
 
+        self.ball.draw(self.win)
 
+    
+    # Describe method here
+    def init_Paddle(self):
+        
+        paddleWidth = float(self.window_width) / 20.0 
+        paddleHeight = float(self.window_height) / 100.0
+        
+        paddle_Y = self.window_height * 0.80
+
+        x1 = ( float(self.window_width)  / 2.0) - (paddleWidth / 2.0)
+        #y1 =  
+
+        x2 = ( float(self.window_width)  / 2.0) + (paddleWidth / 2.0)
+        #y2 = 
+
+        #self.graphic = Rectangle(Point(x1, y1), Point(x2, y2) )
+        #self.paddle.setFill("purple")
+
+        #self.paddle.draw(self.win)
 
     # describe method here
     def drawBricks(self):
@@ -88,8 +180,6 @@ class game:
 
         #  Set up the intial configuration
         self.bricks = []
-        self.ball = Circle(Point(3,4), 10.5)
-
 
         # Display your face
         # Take screen shot of face when you lose??
@@ -98,7 +188,10 @@ class game:
         # Set up the bricks
         self.init_Bricks() 
         self.drawBricks()
-        
+                    
+        self.ball = Ball(self)
+        self.paddle = Paddle(self)
+            
 
     # Describe function here
     def getUserInput(self):
